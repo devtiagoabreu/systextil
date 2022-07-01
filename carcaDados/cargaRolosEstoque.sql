816178904b40a8dc3c6ec36b681594ce394a161b
Tiago Abreu <devtiagoabreu@gmail.com>
2022-07-01 16:13:36 -0300

    NÚMERO DE ROLOS POR GAVETA

--- carcaDados/cargaRolosEstoque.sql
+++ carcaDados/cargaRolosEstoque.sql
@@ -27,3 +27,36 @@ GROUP BY
     Desenho, 
     Variante, 
     Categoria
+
+--
+
+SELECT
+    CP.Nro_Rolo,
+    CP.Nro_Peca,
+	CP.Produto AS ProdutoCodigo,
+	Produtos.Descricao AS ProdutoDescricao,
+	CP.Situacao,
+	CP.Cor,
+	Car_Cores.Descricao AS CorDescricao,
+	CP.Desenho,
+	CP.Variante,
+	Categoria_Tinto AS Categoria,
+	ISNULL(CP.Metros,0) AS Metros
+FROM 
+	DBMicrodata.dbo.Cte_Peca CP 
+	LEFT JOIN DBMicrodata.dbo.CTE_Baixa CB ON CP.Empresa=CB.Empresa and CP.Situacao=CB.Situacao and CP.Nro_Rolo=CB.Nro_Rolo and CP.Nro_Peca=CB.Nro_Peca
+	INNER JOIN DBMicrodata.dbo.Produtos ON Produtos.Codigo = CP.Produto
+	INNER JOIN DBMicrodata.dbo.Produtos_Tecidos ON Produtos_Tecidos.Produto = CP.Produto
+	INNER JOIN DBMicrodata.dbo.Car_Cores ON Car_Cores.Codigo = CP.Cor
+WHERE
+	CP.Gaveta IN ('013') AND
+	CP.Empresa like '01' AND 
+	CP.Situacao like '%' AND 
+	CP.Nro_Rolo like '%' AND 
+	CP.Nro_Peca like '%' AND 
+	CP.Produto like '%' AND 
+	Cor like '%' AND 
+	Desenho like '%' AND 
+	Categoria_Tinto Like '%' AND 
+	IsNull(Variante, '') Like '%' AND
+	CB.Data_Saida IS NULL
