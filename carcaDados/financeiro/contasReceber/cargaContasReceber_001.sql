d76e10cb67fae2c0d6a4182d73a84e705c441217
Tiago Abreu <devtiagoabreu@gmail.com>
2022-08-11 08:46:02 -0300

    INSERINDO TIPO 9 EM TIPO DE COMISSÃO

---
+++ carcaDados/financeiro/contasReceber/cargaContasReceber_001.sql
@@ -0,0 +1,68 @@
+SELECT 
+	NP.Empresa_Parcelas AS CODIGO_EMPRESA,
+	SUBSTRING(REPLACE(REPLACE(REPLACE(NP.Cliente,'.',''),'/',''),'-',''),1,8) AS CLI_DUP_CGC_CLI9,
+	SUBSTRING(REPLACE(REPLACE(REPLACE(NP.Cliente,'.',''),'/',''),'-',''),9,4) AS CLI_DUP_CGC_CLI4,
+	SUBSTRING(REPLACE(REPLACE(REPLACE(NP.Cliente,'.',''),'/',''),'-',''),13,2) AS CLI_DUP_CGC_CLI2,
+	'0' AS TIPO_TITULO,
+	NP.Documento_Parcelas AS NUM_DUPLICATA,
+	NP.Parcela_Parcelas AS SEQ_DUPLICATAS,
+	FORMAT(NP.Vencimento_Parcelas, 'dd/MM/yyyy') AS DATA_VENC_DUPLIC,
+	FORMAT(NP.Vencimento_Parcelas, 'dd/MM/yyyy') AS DATA_PRORROGACAO,
+	FORMAT(NP.Vencimento_Parcelas, 'dd/MM/yyyy') AS DATA_EMISSAO,
+	'' AS PREVISAO, 
+	REPLACE(NP.Valor_Parcelas,'.',',') AS VALOR_DUPLICATA,
+	'0' AS SITUACAO_DUPLIC,
+	NP.Banco_Parcelas AS PORTADOR_DUPLIC,
+	PF.Pedido AS PEDIDO_VENDA,
+	NP.Nosso_Nr_Parcelas AS NR_TITULO_BANCO,
+	NV.Vendedor_NF_Vendedores AS COD_REP_CLIENTE,
+	NV.Inc_Vend_Parcela AS POSICAO_DUPLIC,
+	'' AS COD_HISTORICO,
+	'' AS COMPL_HISTORICO,
+	'1' AS COD_LOCAL,
+	'0' AS MOEDA_TITULO,
+	REPLACE(NP.Valor_Parcelas,'.',',') AS VALOR_MOEDA,
+	'1' AS COD_TRANSACAO,
+	'0' AS CODIGO_CONTABIL,
+	'0' AS NUM_CONTABIL,
+	REPLACE(ISNULL(NP.Valor_Parcelas,0),'.',',') AS SALDO_DUPLICATA,
+	NP.Nr_Conta_Age_Parcelas AS CONTA_CORRENTE,
+	'0' AS NUMERO_REMESSA,
+	REPLACE(RC.Porcentagem_Comissoes,'.',',') AS PERCENTUAL_COMIS,
+	REPLACE(NV.Valor_Comissao_NF_Vend,'.',',') AS VALOR_COMIS,
+	REPLACE(NP.Valor_Parcelas,'.',',') AS BASE_CALC_COMIS,
+	'100' AS PERC_COMIS_CREC,
+	'0' AS TIPO_COMISSAO,
+	'0' AS CODIGO_ADMINISTR,
+	'0' AS COMISSAO_ADMINISTR,
+	'0' AS PERC_COMIS_CREC_ADM,
+	'2' AS SEQ_END_COBRANCA,
+	'' AS OBSERVACAO,
+	SUBSTRING(REPLACE(REPLACE(REPLACE(NP.Cliente,'.',''),'/',''),'-',''),1,8) AS CLI9RESPTIT,
+	SUBSTRING(REPLACE(REPLACE(REPLACE(NP.Cliente,'.',''),'/',''),'-',''),9,4) AS CLI4RESPTIT,
+	SUBSTRING(REPLACE(REPLACE(REPLACE(NP.Cliente,'.',''),'/',''),'-',''),13,2) AS CLI2RESPTIT,
+	'0' AS COD_FORMA_PAGTO
+FROM
+	Notas_Fiscais_Parcelas AS NP
+	INNER JOIN Notas_Fiscais_Vendedores_Parcelas AS NV ON 
+		NV.Doc_NF_Vendedores = NP.Documento_Parcelas AND
+		NV.Serie = NP.Serie AND
+		NV.Inc_Vend_Parcela = NP.Inc_Parcela AND
+		NV.Empresa_NF_Vendedores = NP.Empresa_Parcelas
+	INNER JOIN Rec_Comissoes RC ON RC.tipo_comissoes = NV.Tipo_Comissao_NF_Vend AND
+		RC.Vendedor_Comissoes = NV.Vendedor_NF_Vendedores
+	LEFT JOIN Fat_Pedido AS PF ON 
+	  PF.Nr_Nota = NP.Documento_Parcelas AND
+	  PF.Serie = NP.Serie AND
+	  PF.Cliente = NP.Cliente 
+WHERE
+	NV.Empresa_NF_Vendedores IN ('01') and
+	NP.Vencimento_Parcelas BETWEEN '2022-07-15 00:00:00' AND '2022-07-15 00:00:00' --AND
+	--PF.Data_Nota BETWEEN '2022-07-15 00:00:00' AND '2022-12-31 00:00:00' 
+	--NV.Doc_NF_Vendedores='000003' and 
+	----NV.Parcela_NF_Vendedores='01' and 
+	--NV.Serie='1  55' 
+ORDER BY
+	NP.Documento_Parcelas DESC,
+	NP.Serie,
+	NP.Id_Parcela
