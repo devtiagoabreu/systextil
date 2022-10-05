655c9e8b39d6f8b80ec2fcf134858c2cafe302f8
Tiago Abreu <devtiagoabreu@gmail.com>
2022-10-05 17:20:29 -0300

    CONTAS A PAGAR - TÍTULOS BAIXADOS - EM HOMOLOGAÇÃO

---
+++ etiqueta.sql
@@ -0,0 +1,39 @@
+if tipo_tecido = 2
+then begin 
+    print 'A550,220,1,1,1,1,N,"Cor"'
+    print 'A535,220,1,3,1,1,N,"' /+ prodcai_subgrupo /+ '"'
+
+    print 'A550,230,1,1,1,1,N,"Cor\Estampa:"'
+    print 'A550,230,1,2,2,1,N,"' /+ prodcai_item /+ '"'
+end 
+else begin 
+    print 'A550,220,1,1,1,1,N,"Cor"'
+    print 'A535,220,1,3,1,1,N,"' /+ prodcai_item /+ '"'
+end
+   
+
+
+if tamanho_ref = 'CRU'
+ then begin
+    print 'A550,220,1,1,1,1,N,"Sit"'
+    print 'A535,220,1,3,1,1,N,"' /+ prodcai_subgrupo /+ '"'
+
+    print 'A550,230,1,1,1,1,N,"Cor\Estampa:"'
+    print 'A550,230,1,2,2,1,N,"' /+ prodcai_item /+ '"'
+end 
+else if tamanho_ref = 'TIN'
+then begin
+    print 'A550,220,1,1,1,1,N,"Sit"'
+    print 'A535,220,1,3,1,1,N,"' /+ prodcai_subgrupo /+ '"'
+
+    print 'A550,230,1,1,1,1,N,"Cor\Estampa:"'
+    print 'A550,230,1,2,2,1,N,"' /+ prodcai_item /+ '"'
+end
+else begin 
+    print 'A550,220,1,1,1,1,N,"Cor"'
+    print 'A535,220,1,3,1,1,N,"' /+ prodcai_subgrupo /+ '"'
+
+    print 'A550,230,1,1,1,1,N,"Cor\Estampa:"'
+    print 'A550,230,1,2,2,1,N,"' /+ prodcai_item /+ '"'
+end
+   
